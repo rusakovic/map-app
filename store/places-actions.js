79aa36f318c037053188e3109f0536b9ba868939
rusakovic <maxrusakovich@gmail.com>
2020-02-13 20:47:18 +0300

    adding imageUri path to redux store

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,24 @@
+import * as FileSystem from 'expo-file-system'
+
 export const ADD_PLACE = 'ADD_PLACE'
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } }
+  return async dispatch => {
+    // take the link to image and leave only file name
+    const fileName = image.split('/').pop()
+    const newPath = FileSystem.documentDirectory + fileName
+
+    //move image from temporary folder to our folder
+    try {
+      FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } })
+  }
 }
